c4d1acb62c8b5277afb6a4aefb6ea2c1557d54fc
jordand2003 <jordandominguez600@gmail.com>
2023-10-24 18:53:36 -0400

    added input field for new to-do items

--- to-do-list/src/components/Body.js
+++ to-do-list/src/components/Body.js
@@ -2,19 +2,39 @@ import React from "react";
 
 export default function Body() {
   const [items, setItems] = React.useState([]);
+  const [formData, setFormData] = React.useState({
+    item: "",
+  });
 
   function handleClick() {
-    const newItems = `Note ${items.length + 1}`;
+    const newItems = `${formData}`;
     setItems((prevItems) => [...prevItems, newItems]);
   }
 
-  const itemsElements = items.map((item) => <li className="item">{item}</li>);
+  function handleChange(event) {
+    event.preventDefault();
+    setFormData(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  const itemsElements = items.map((item) => <li className="item">{items}</li>);
 
   return (
     <div>
       <button className="new--item" onClick={handleClick}>
-        New Note +{" "}
+        New Note +
       </button>
+      <input
+        type="text"
+        placeholder="Insert errand here"
+        className="item"
+        name="item"
+        value={formData.item}
+        onChange={handleChange}
+      ></input>
       <ul>{itemsElements}</ul>
     </div>
   );
