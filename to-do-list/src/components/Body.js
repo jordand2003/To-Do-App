2831b471773585de0fa22e47283ddd12ad774cc2
jordand2003 <jordandominguez600@gmail.com>
2023-10-24 20:58:29 -0400

    bug fix for new list items + styling updates

--- to-do-list/src/components/Body.js
+++ to-do-list/src/components/Body.js
@@ -1,31 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Body() {
-  const [items, setItems] = React.useState([]);
-  const [formData, setFormData] = React.useState({
+  const [items, setItems] = useState([]);
+  const [formData, setFormData] = useState({
     item: "",
   });
 
   function handleClick() {
-    const newItems = `${formData}`;
-    setItems((prevItems) => [...prevItems, newItems]);
+    if (formData.item.trim() !== "") {
+      // Check if the input is not empty
+      setItems((prevItems) => [...prevItems, formData.item]);
+      setFormData({ item: "" }); // Clear the input field
+    }
   }
 
   function handleChange(event) {
-    event.preventDefault();
-    setFormData(event.target.value);
+    setFormData({ item: event.target.value });
   }
 
   function handleSubmit(event) {
     event.preventDefault();
   }
 
-  const itemsElements = items.map((item) => <li className="item">{items}</li>);
+  const itemsElements = items.map((item) => <li className="item">{item}</li>);
 
   return (
     <div>
       <button className="new--item" onClick={handleClick}>
-        New Note +
+        Add +
       </button>
       <input
         type="text"
@@ -34,8 +36,9 @@ export default function Body() {
         name="item"
         value={formData.item}
         onChange={handleChange}
-      ></input>
+      />
       <ul>{itemsElements}</ul>
+      <p className="bottom">Remaining Tasks: {items.length}</p>
     </div>
   );
 }
